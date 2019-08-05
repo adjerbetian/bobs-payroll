fcc167259ad267ae3fcb7b6460920c64c7e1d17a
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2019-08-05 23:39:54 +0200

    added missing tests

--- src/mongo/timeCardRepository.spec.ts
+++ src/mongo/timeCardRepository.spec.ts
@@ -2,8 +2,37 @@ import "../../test/integrationTest";
 import { expect } from "../../test/unitTest";
 import { generateTimeCard } from "../../test/generators";
 import { timeCardRepository } from "./timeCardRepository";
+import { TimeCard } from "../entities";
+import { dbTimeCards } from "./db";
+import * as _ from "lodash";
 
 describe("timeCardRepository", () => {
+    describe("fetchAllOfEmployee", () => {
+        it("should return all the employee's time cards", async () => {
+            const salesReceipt = await dbGenerateTimeCard();
+
+            const salesReceipts = await timeCardRepository.fetchAllOfEmployee(
+                salesReceipt.employeeId
+            );
+
+            expect(salesReceipts).to.deep.equal([salesReceipt]);
+        });
+        it("should not return other employees' time cards", async () => {
+            const salesReceipt = await dbGenerateTimeCard();
+
+            const salesReceipts = await timeCardRepository.fetchAllOfEmployee(
+                salesReceipt.employeeId + 1
+            );
+
+            expect(salesReceipts).to.be.empty;
+        });
+
+        async function dbGenerateTimeCard(): Promise<TimeCard> {
+            const timeCard = generateTimeCard();
+            await dbTimeCards.insertOne(_.cloneDeep(timeCard));
+            return timeCard;
+        }
+    });
     describe("insertOne", () => {
         it("insert the given employee", async () => {
             const timeCard = generateTimeCard();
