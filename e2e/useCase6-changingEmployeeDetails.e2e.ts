35b93373eef55fc2583140f3d9f82a4b286827c1
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2019-08-11 10:36:26 +0200

    use case 6 - 2/13 - red

--- e2e/useCase6-changingEmployeeDetails.e2e.ts
+++ e2e/useCase6-changingEmployeeDetails.e2e.ts
@@ -11,18 +11,25 @@ describe("Use Case 6: Changing Employee Details", () => {
         const dbEmployee = await mongoEmployeeRepository.fetchById(employee.id);
         expect(dbEmployee.name).to.equal("James Bond");
     });
-    it("should change the employee's address", async () => {});
-    it("should change the employee to hourly rate", async () => {});
-    it("should change the employee to monthly salary", async () => {});
-    it("should change the employee to commissioned", async () => {});
-    it("should change the employee to hold", async () => {});
-    it("should change the employee's direct deposit infos ", async () => {});
-    it("should change the employee's paycheck mail ", async () => {});
-    it("should put the employee in Union", async () => {});
-    it("should remove the employee from Union", async () => {});
+    it("should change the employee's address", async () => {
+        const employee = await seedHourlyRateEmployee();
+
+        await executePayrollCommand(`ChgEmp ${employee.id} Address "my new address"`);
+
+        const dbEmployee = await mongoEmployeeRepository.fetchById(employee.id);
+        expect(dbEmployee.address).to.equal("my new address");
+    });
+    it.skip("should change the employee to hourly rate", async () => {});
+    it.skip("should change the employee to monthly salary", async () => {});
+    it.skip("should change the employee to commissioned", async () => {});
+    it.skip("should change the employee to hold", async () => {});
+    it.skip("should change the employee's direct deposit.skip infos ", async () => {});
+    it.skip("should change the employee's paycheck mail ", async () => {});
+    it.skip("should put the employee in Union", async () => {});
+    it.skip("should remove the employee from Union", async () => {});
     describe("on error", () => {
-        it("should do nothing when transaction is wrong", async () => {});
-        it("should do nothing when the employee does not exist", async () => {});
-        it("should do nothing when the union member id is already used", async () => {});
+        it.skip("should do nothing when transaction is wrong", async () => {});
+        it.skip("should do nothing when the employee does not exist", async () => {});
+        it.skip("should do nothing when the union member id is already used", async () => {});
     });
 });
