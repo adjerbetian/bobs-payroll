1cba61425f2b401e318cb5064d566574216b05f3
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2019-08-25 14:36:21 +0200

    refactored tests

--- e2e/useCase6-changingEmployeeDetails.e2e.ts
+++ e2e/useCase6-changingEmployeeDetails.e2e.ts
@@ -1,77 +1,100 @@
-import { EmployeeType, mongoEmployeeRepository, mongoPaymentMethodRepository, PaymentMethodType } from "../src";
+import {
+    Employee,
+    EmployeeType,
+    mongoEmployeeRepository,
+    mongoPaymentMethodRepository,
+    PaymentMethodType
+} from "../src";
 import { executePayrollCommand, expect } from "../test/e2eTest";
 import { seedDirectPaymentMethod, seedHourlyEmployee, seedSalariedEmployee } from "../test/seeders";
+import { generateIndex } from "../test/utils";
 
 describe("Use Case 6: Changing Employee Details", () => {
-    it("should change the employee's name", async () => {
-        const employee = await seedHourlyEmployee();
+    let employee: Employee;
 
-        await executePayrollCommand(`ChgEmp ${employee.id} Name "James Bond"`);
-
-        const dbEmployee = await mongoEmployeeRepository.fetchById(employee.id);
-        expect(dbEmployee.name).to.equal("James Bond");
+    beforeEach(async () => {
+        employee = await seedHourlyEmployee();
     });
-    it("should change the employee's address", async () => {
-        const employee = await seedHourlyEmployee();
 
-        await executePayrollCommand(`ChgEmp ${employee.id} Address "my new address"`);
+    describe("basic infos", () => {
+        it("should change the employee's name", async () => {
+            await executePayrollCommand(`ChgEmp ${employee.id} Name "James Bond"`);
 
-        const dbEmployee = await mongoEmployeeRepository.fetchById(employee.id);
-        expect(dbEmployee.address).to.equal("my new address");
+            const dbEmployee = await mongoEmployeeRepository.fetchById(employee.id);
+            expect(dbEmployee.name).to.equal("James Bond");
+        });
+        it("should change the employee's address", async () => {
+            await executePayrollCommand(`ChgEmp ${employee.id} Address "my new address"`);
+
+            const dbEmployee = await mongoEmployeeRepository.fetchById(employee.id);
+            expect(dbEmployee.address).to.equal("my new address");
+        });
+        it("should do nothing when the employee does not exist", async () => {
+            const nonExistingId = generateIndex();
+
+            await executePayrollCommand(`ChgEmp ${nonExistingId} Address "my new address"`);
+
+            const dbEmployee = await mongoEmployeeRepository.fetchById(employee.id);
+            expect(dbEmployee).to.deep.equal(employee);
+        });
     });
-    it("should change the employee to hourly", async () => {
-        const employee = await seedSalariedEmployee();
+    describe("type", () => {
+        it("should change the employee to hourly", async () => {
+            employee = await seedSalariedEmployee();
 
-        await executePayrollCommand(`ChgEmp ${employee.id} Hourly 10`);
+            await executePayrollCommand(`ChgEmp ${employee.id} Hourly 10`);
 
-        const dbEmployee = await mongoEmployeeRepository.fetchById(employee.id);
-        expect(dbEmployee).to.have.property("type", EmployeeType.HOURLY);
-        expect(dbEmployee).to.have.property("hourlyRate", 10);
-        expect(dbEmployee).not.to.have.property("monthlySalary");
+            const dbEmployee = await mongoEmployeeRepository.fetchById(employee.id);
+            expect(dbEmployee).to.have.property("type", EmployeeType.HOURLY);
+            expect(dbEmployee).to.have.property("hourlyRate", 10);
+            expect(dbEmployee).not.to.have.property("monthlySalary");
+        });
+        it("should change the employee to monthly salary", async () => {
+            employee = await seedHourlyEmployee();
+
+            await executePayrollCommand(`ChgEmp ${employee.id} Salaried 10`);
+
+            const dbEmployee = await mongoEmployeeRepository.fetchById(employee.id);
+            expect(dbEmployee).to.have.property("type", EmployeeType.SALARIED);
+            expect(dbEmployee).to.have.property("monthlySalary", 10);
+            expect(dbEmployee).not.to.have.property("hourlyRate");
+        });
+        it("should change the employee to commissioned", async () => {
+            employee = await seedHourlyEmployee();
+
+            await executePayrollCommand(`ChgEmp ${employee.id} Commissioned 10 30`);
+
+            const dbEmployee = await mongoEmployeeRepository.fetchById(employee.id);
+            expect(dbEmployee).to.have.property("type", EmployeeType.COMMISSIONED);
+            expect(dbEmployee).to.have.property("monthlySalary", 10);
+            expect(dbEmployee).to.have.property("commissionRate", 30);
+            expect(dbEmployee).not.to.have.property("hourlyRate");
+        });
     });
-    it("should change the employee to monthly salary", async () => {
-        const employee = await seedHourlyEmployee();
+    describe("payment method", () => {
+        it("should set the employee's payment method to hold", async () => {
+            await executePayrollCommand(`ChgEmp ${employee.id} Hold`);
 
-        await executePayrollCommand(`ChgEmp ${employee.id} Salaried 10`);
+            const dbPaymentMethod = await mongoPaymentMethodRepository.fetchByEmployeeId(employee.id);
+            expect(dbPaymentMethod).to.have.property("type", PaymentMethodType.HOLD);
+        });
+        it("should change the employee's payment method to hold when was direct", async () => {
+            await seedDirectPaymentMethod({ employeeId: employee.id });
 
-        const dbEmployee = await mongoEmployeeRepository.fetchById(employee.id);
-        expect(dbEmployee).to.have.property("type", EmployeeType.SALARIED);
-        expect(dbEmployee).to.have.property("monthlySalary", 10);
-        expect(dbEmployee).not.to.have.property("hourlyRate");
+            await executePayrollCommand(`ChgEmp ${employee.id} Hold`);
+
+            const dbPaymentMethod = await mongoPaymentMethodRepository.fetchByEmployeeId(employee.id);
+            expect(dbPaymentMethod).to.have.property("type", PaymentMethodType.HOLD);
+            expect(dbPaymentMethod).not.to.have.property("bank");
+        });
+        it.skip("should set the employee's payment method to direct deposit", async () => {});
+        it.skip("should change the employee's payment method to direct deposit when was hold", async () => {});
+        it.skip("should set the employee's payment method to paycheck mail ", async () => {});
+        it.skip("should change the employee's payment method to paycheck mail when was hold", async () => {});
     });
-    it("should change the employee to commissioned", async () => {
-        const employee = await seedHourlyEmployee();
-
-        await executePayrollCommand(`ChgEmp ${employee.id} Commissioned 10 30`);
-
-        const dbEmployee = await mongoEmployeeRepository.fetchById(employee.id);
-        expect(dbEmployee).to.have.property("type", EmployeeType.COMMISSIONED);
-        expect(dbEmployee).to.have.property("monthlySalary", 10);
-        expect(dbEmployee).to.have.property("commissionRate", 30);
-        expect(dbEmployee).not.to.have.property("hourlyRate");
+    describe("Union", () => {
+        it.skip("should put the employee in Union", async () => {});
+        it.skip("should remove the employee from Union", async () => {});
+        it.skip("should do nothing when the union member id is already used", async () => {});
     });
-    it("should set the employee's payment method to hold", async () => {
-        const employee = await seedHourlyEmployee();
-
-        await executePayrollCommand(`ChgEmp ${employee.id} Hold`);
-
-        const dbPaymentMethod = await mongoPaymentMethodRepository.fetchByEmployeeId(employee.id);
-        expect(dbPaymentMethod).to.have.property("type", PaymentMethodType.HOLD);
-    });
-    it("should change the employee's payment method to hold when was direct", async () => {
-        const employee = await seedHourlyEmployee();
-        await seedDirectPaymentMethod({ employeeId: employee.id });
-
-        await executePayrollCommand(`ChgEmp ${employee.id} Hold`);
-
-        const dbPaymentMethod = await mongoPaymentMethodRepository.fetchByEmployeeId(employee.id);
-        expect(dbPaymentMethod).to.have.property("type", PaymentMethodType.HOLD);
-        expect(dbPaymentMethod).not.to.have.property("bank");
-    });
-    it.skip("should change the employee's direct deposit.skip infos ", async () => {});
-    it.skip("should change the employee's paycheck mail ", async () => {});
-    it.skip("should put the employee in Union", async () => {});
-    it.skip("should remove the employee from Union", async () => {});
-    it.skip("should do nothing when the union member id is already used", async () => {});
-    it.skip("should do nothing when the employee does not exist", async () => {});
 });
