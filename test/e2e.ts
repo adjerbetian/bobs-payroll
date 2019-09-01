2c0d1d6e612ee03dc174d25634e237ad4783021f
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2019-09-01 13:45:12 +0200

    removed compilation of test files in build

--- test/e2e.ts
+++ test/e2e.ts
@@ -4,10 +4,10 @@ export * from "./integration";
 export * from "./utils/execute";
 
 before(async () => {
-    await execute("IF EXIST dist\\ DEL /F/Q/S dist\\*.*");
+    await execute(`IF EXIST dist\\ RMDIR dist /S /Q`);
     await execute("npm run build");
 });
 
 export async function executePayrollCommand(command: string): Promise<string> {
-    return await execute("node dist/src/main.js " + command);
+    return await execute("node dist/main.js " + command);
 }
