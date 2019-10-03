764063242d888a44bf0424a5475b8ba0135ff725
Alexandre Djerbetian <alexandre.djerbetian@gmail.com>
2019-10-03 23:53:10 +0200

    cucumber: finalisation

--- src/test/e2e.ts
+++
@@ -1,9 +0,0 @@
-import { e2eHooks, mapHooksToMocha } from "./hooks";
-import { execute } from "./utils";
-
-mapHooksToMocha(e2eHooks);
-
-export * from "./integration";
-export async function executePayrollCommand(command: string): Promise<string> {
-    return await execute("node dist/main.js " + command);
-}
